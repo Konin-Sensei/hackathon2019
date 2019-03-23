8c6066de55c4b3f473f5aa384ca566694bfcc92d
Konin_Sensei <dmsconway@gmail.com>
2019-03-23 12:45:08 -0400

    Update car.h

---
+++ car.h
@@ -0,0 +1,18 @@
+#ifndef CAR_H
+#define CAR_H
+
+#include <string>
+#include "driver.h"
+
+class Car{
+public:
+
+    
+private:
+    //Driver::Driver[] list_of_drivers;
+    std::string car_make;
+    std::string car_model;
+    
+};
+
+#endif /* BOOK_H */
